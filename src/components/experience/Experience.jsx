327c32aeae0a164e5b20466879e208b26dc816a8
Eduard Cruz <salikintres2001@gmail.com>
2022-07-18 19:25:41 -0500

    [Update] Se termina el reemplazo de comillas simples a comillas dobles

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './experience.css';
+import React from "react";
+import "./experience.css";
 import {
     SiHtml5,
     SiCss3,
@@ -13,13 +13,13 @@ import {
     SiMongodb,
     SiMysql,
     SiPostman
-} from 'react-icons/si';
-import { useTranslation } from 'react-i18next';
+} from "react-icons/si";
+import { useTranslation } from "react-i18next";
 
 const Experience = () => {
     const [t] = useTranslation("global");
     return (
-        <section id='experience'>
+        <section id="experience">
             <h5>{t("experience.title")}</h5>
             <h2>{t("experience.subtitle")}</h2>
 
@@ -28,45 +28,45 @@ const Experience = () => {
                     <h3>{t("experience.front.title")}</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <SiHtml5 className='experience__details-icon' />
+                            <SiHtml5 className="experience__details-icon" />
                             <div>
                                 <h4>HTML</h4>
-                                <small className='text-light'>{t("experience.front.f1.xp")}</small>
+                                <small className="text-light">{t("experience.front.f1.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiCss3 className='experience__details-icon' />
+                            <SiCss3 className="experience__details-icon" />
                             <div>
                                 <h4>CSS</h4>
-                                <small className='text-light'>{t("experience.front.f2.xp")}</small>
+                                <small className="text-light">{t("experience.front.f2.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiJavascript className='experience__details-icon' />
+                            <SiJavascript className="experience__details-icon" />
                             <div>
                                 <h4>JavaScript</h4>
-                                <small className='text-light'>{t("experience.front.f3.xp")}</small>
+                                <small className="text-light">{t("experience.front.f3.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiReact className='experience__details-icon' />
+                            <SiReact className="experience__details-icon" />
                             <div>
                                 <h4>ReactJS</h4>
-                                <small className='text-light'>{t("experience.front.f4.xp")}</small>
+                                <small className="text-light">{t("experience.front.f4.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiVuedotjs className='experience__details-icon' />
+                            <SiVuedotjs className="experience__details-icon" />
                             <div>
                                 <h4>VueJS</h4>
-                                <small className='text-light'>{t("experience.front.f5.xp")}</small>
+                                <small className="text-light">{t("experience.front.f5.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiSvelte className='experience__details-icon' />
+                            <SiSvelte className="experience__details-icon" />
                             <div>
                                 <h4>Svelte</h4>
-                                <small className='text-light'>{t("experience.front.f6.xp")}</small>
+                                <small className="text-light">{t("experience.front.f6.xp")}</small>
                             </div>
                         </article>
                     </div>
@@ -75,45 +75,45 @@ const Experience = () => {
                     <h3>{t("experience.back.title")}</h3>
                     <div className="experience__content">
                         <article className="experience__details">
-                            <SiGo className='experience__details-icon' />
+                            <SiGo className="experience__details-icon" />
                             <div>
                                 <h4>Golang</h4>
-                                <small className='text-light'>{t("experience.back.b1.xp")}</small>
+                                <small className="text-light">{t("experience.back.b1.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiPython className='experience__details-icon' />
+                            <SiPython className="experience__details-icon" />
                             <div>
                                 <h4>Python</h4>
-                                <small className='text-light'>{t("experience.back.b2.xp")}</small>
+                                <small className="text-light">{t("experience.back.b2.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiNodedotjs className='experience__details-icon' />
+                            <SiNodedotjs className="experience__details-icon" />
                             <div>
                                 <h4>NodeJS</h4>
-                                <small className='text-light'>{t("experience.back.b3.xp")}</small>
+                                <small className="text-light">{t("experience.back.b3.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiMongodb className='experience__details-icon' />
+                            <SiMongodb className="experience__details-icon" />
                             <div>
                                 <h4>MongoDB</h4>
-                                <small className='text-light'>{t("experience.back.b4.xp")}</small>
+                                <small className="text-light">{t("experience.back.b4.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiMysql className='experience__details-icon' />
+                            <SiMysql className="experience__details-icon" />
                             <div>
                                 <h4>MySQL</h4>
-                                <small className='text-light'>{t("experience.back.b5.xp")}</small>
+                                <small className="text-light">{t("experience.back.b5.xp")}</small>
                             </div>
                         </article>
                         <article className="experience__details">
-                            <SiPostman className='experience__details-icon' />
+                            <SiPostman className="experience__details-icon" />
                             <div>
                                 <h4>Postman</h4>
-                                <small className='text-light'>{t("experience.back.b6.xp")}</small>
+                                <small className="text-light">{t("experience.back.b6.xp")}</small>
                             </div>
                         </article>
                     </div>
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import './footer.css';
+import React from "react";
+import "./footer.css";
 import {
     BsLinkedin,
     BsGithub,
     BsInstagram,
     BsWhatsapp
-} from 'react-icons/bs';
-import { useTranslation } from 'react-i18next';
+} from "react-icons/bs";
+import { useTranslation } from "react-i18next";
 
 const Footer = () => {
     const [t] = useTranslation("global");
     return (
         <footer>
-            <a href="/#" className='footer__logo'>dev_ecorp</a>
-            <ul className='permalinks'>
+            <a href="/#" className="footer__logo">dev_ecorp</a>
+            <ul className="permalinks">
                 <li>
                     <a href="/#">{t("footer.home")}</a>
                 </li>
@@ -36,7 +36,7 @@ const Footer = () => {
                 <a href="https://wa.link/3dj9gk" target="_blank" rel="noreferrer noopener"><BsWhatsapp /></a>
                 <a href="https://www.instagram.com/bug.buster_" target="_blank" rel="noreferrer noopener"><BsInstagram /></a>
             </div>
-            <div className='footer__copyright'>
+            <div className="footer__copyright">
                 <small>&copy; {t("footer.copy")}</small>
             </div>
         </footer>
