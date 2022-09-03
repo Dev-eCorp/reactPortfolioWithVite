7ca0feb16e06b62845cdae0cd7fccee67af6f7dd
Eduard Cruz <salikintres2001@gmail.com>
2022-09-03 16:43:41 -0500

    [Update] Se modifica header/CTA para que no descargue los archivos sino que abra el documento en drive

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,14 @@
-import CVEng from "../../assets/pdf/EnglishCurriculumEduardFerneyCruzOspina.pdf";
-import CVEsp from "../../assets/pdf/CurriculumEduardFerneyCruzOspina.pdf";
+// import CVEng from "../../assets/pdf/EnglishCurriculumEduardFerneyCruzOspina.pdf";
+// import CVEsp from "../../assets/pdf/CurriculumEduardFerneyCruzOspina.pdf";
 import { useTranslation } from "react-i18next";
 
 const CTA = () => {
     const [t] = useTranslation("global");
+    const linkCVEng = "https://drive.google.com/file/d/1o_rA88mYDZF_5YKuapFVMM6k6JiL8b_K/view?usp=sharing";
+    const linkCVEsp = "https://drive.google.com/file/d/1YfuAi5HohSn-cgHJSOp9eCJdTpo19G79/view?usp=sharing";
     return (
         <div className="cta">
-            <a href={t("header.cta.cvOption") == "CVEng" ? CVEng : CVEsp} download className="btn">{t("header.cta.cv")}</a>
+            <a href={t("header.cta.cvOption") == "CVEng" ? linkCVEng : linkCVEsp} download className="btn" target="_blank">{t("header.cta.cv")}</a>
             <a href="#contact" className="btn btn-primary">{t("header.cta.contact")}</a>
         </div>
     )
