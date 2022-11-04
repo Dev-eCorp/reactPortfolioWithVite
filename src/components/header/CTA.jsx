aa6af3cff8c3e06fd036433fe642459dc616ce69
Eduard Cruz <salikintres2001@gmail.com>
2022-11-04 15:19:32 -0500

    [Update] Se actualizan links de portafolio

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import { useTranslation } from "react-i18next";
 
 const CTA = () => {
     const [t] = useTranslation("global");
-    const linkCVEng = "https://drive.google.com/file/d/1o_rA88mYDZF_5YKuapFVMM6k6JiL8b_K/view?usp=sharing";
-    const linkCVEsp = "https://drive.google.com/file/d/1YfuAi5HohSn-cgHJSOp9eCJdTpo19G79/view?usp=sharing";
+    const linkCVEng = "https://drive.google.com/file/d/1SnYWGkVyCaOsyP_ayTHR4DsIpItNR5xj/view?usp=sharing";
+    const linkCVEsp = "https://drive.google.com/file/d/1gq4TCi_SR20EGazk2WyaVlMmbZaIyF2A/view?usp=sharing";
     return (
         <div className="cta">
             <a href={t("header.cta.cvOption") == "CVEng" ? linkCVEng : linkCVEsp} download className="btn" target="_blank">{t("header.cta.cv")}</a>
